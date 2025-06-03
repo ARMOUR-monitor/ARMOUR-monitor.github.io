d527fe567b23460a4fac9b7bf2013f3c9fd0843a
Coming98 <jcqueue@gmail.com>
2025-06-03 14:57:40 -0400

    Initial Commit

---
+++ svelte.config.js
@@ -0,0 +1,18 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.argv.includes('dev');
+
+export default {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
+		paths: { base: dev ? '' : '/' }
+	}
+};
